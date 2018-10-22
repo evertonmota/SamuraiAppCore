4f668ca88ea9537d44f6d7e9870d0814b99e309d
Everton Mota <mcostagt@gmail.com>
2018-10-22 11:03:54 -0300

    First Commit

---
+++ SamuraiApp.Data/SamuraiDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiDbContext: DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        public SamuraiDbContext(DbContextOptions<SamuraiDbContext> options) : base(options)
+        {
+
+        }
+
+        /*
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb; Database= SamuraiAppCoreDb; Trusted_Connection=True; ");
+        }
+        */
+    }
+}
